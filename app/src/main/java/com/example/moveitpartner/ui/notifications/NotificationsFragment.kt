a4c2ad7c1ae01ff6ebbca432dda7fd45b4c693c7
xhbear <xionghsinh106888@gmail.com>
2022-08-25 15:21:47 +0800

    Fix: corrected nav menu display text.

--- app/src/main/java/com/example/moveitpartner/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/moveitpartner/ui/notifications/NotificationsFragment.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.moveitpartner.databinding.FragmentNotificationsBinding
 import com.example.moveitpartner.model.viewmodel.HomeViewModel
-import com.example.moveitpartner.model.viewmodel.NotificationsViewModel
 
 class NotificationsFragment : Fragment() {
 
